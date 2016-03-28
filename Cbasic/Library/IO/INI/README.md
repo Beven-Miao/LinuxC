fc12a98c0108c0e0a16c80772bc12944a5e08e3a
beven <bevenmiao@gmail.com>
2016-03-27 23:28:05 -0400

    Added INI

---
+++ Cbasic/Library/IO/INI/README.md
@@ -0,0 +1,5 @@
+#将INI文件转化成XML文件
+
+注意fseek(*fp,1,SEEK_CUR)
+文件指针向后移一位
+
---
+++ Cbasic/Library/IO/INI/int.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+	FILE *fIni = fopen("Ini","r");
+	FILE *fXml = fopen("Xml","aw+");
+	char buf[100];
+	char dbuf[100];
+	char  ch;
+	int i = 0;
+	char *key,*value;
+
+	if((fIni = fopen("Ini","r")) == NULL){
+		perror("open Ini error");
+		exit(-1);
+	}
+
+	if((fXml = fopen("Xml","w")) == NULL ){
+		perror("open Xml error");
+		exit(-1);
+	}
+
+	while((ch = fgetc(fIni)) !=EOF){
+		if(ch == ';'){
+			i = 0;
+			memset(buf,0,sizeof(buf));
+			while((ch = fgetc(fIni)) != '\n')
+				buf[i++] = ch;
+			fprintf(fXml,"<!--%s-->\n",buf);
+			continue;
+		}
+
+		if(ch == '['){
+			i=0;
+			memset(buf,0,sizeof(buf));
+			while((ch = fgetc(fIni)) != ']')
+				buf[i++] = ch;
+			fseek(fIni,1,SEEK_CUR);
+			fprintf(fXml,"<%s>\n",buf);
+			continue;
+		}
+
+		//该条件判断放在最后，最低优先级
+		if(ch != '\n' && ch != ' '){
+			i = 0;
+			memset(dbuf,0,sizeof(dbuf));
+			dbuf[i++] = ch;//先取第一个字符
+			while((ch = fgetc(fIni)) != '\n')
+				dbuf[i++] = ch;
+			key = strtok(dbuf,"=");
+			value = strtok(NULL, "=");
+			fprintf(fXml, "<%s>%s</%s>\n",key,value,key);
+			continue;
+		}
+
+		if(ch == '\n')
+			fprintf(fXml,"</%s>\n",buf);
+	}
+	fprintf(fXml,"</%s>\n",buf);
+
+	fclose(fXml);
+	fclose(fIni);
+	
+	return 0;
+}
